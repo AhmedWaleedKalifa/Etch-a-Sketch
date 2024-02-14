29c10a8459ca1f2be9d49f7fa7cbafd04e63b863
AhmedWaleedKalifa <ahmed2210waleed@gmail.com>
2024-02-14 20:36:47 +0200

    Feat: add more features like some normal colors to pick easyily and clear button

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,34 @@
 const container=document.querySelector("#container");
 const btn=document.querySelector("#btn");
 const btn2=document.querySelector("#btn2");
+const clear=document.querySelector("#clear");
+const colors=document.querySelector("#colors");
+const colorDiv=Array.from(document.querySelectorAll("#colors div"));
+let color="black";
+
 const arr=[];
 let num=16;
+let myArr=Array.from(document.querySelectorAll("#container div"));
 let mouseDown = 0;
 document.body.onmousedown = () => mouseDown = 1;
 document.body.onmouseup = () => mouseDown = 0;
+for(let i=0;i<(num*num);i++){
+    arr[i]=document.createElement("div");
+    arr[i].setAttribute("style",`width:calc(70svw/${num});height:calc(70svw/${num});`);
+    container.appendChild(arr[i]);
+}
+arr.forEach(element=>{
+    element.addEventListener("mouseover",(e)=>{
+        if(mouseDown){
+            element.style.background=color;
+        }
+    });
+});
 btn.onclick=function(){
+    for(let i=0;i<(num*num);i++){
+        container.removeChild(arr[i]);
+    }
      num=prompt("Enter the number N*N of the grid");
-     let myArr=Array.from(document.querySelectorAll("#container div"));
-     myArr.forEach(element=>{
-        container.removeChild(element);
-     });
      for(let i=0;i<(num*num);i++){
         arr[i]=document.createElement("div");
         arr[i].setAttribute("style",`width:calc(70svw/${num});height:calc(70svw/${num});`);
@@ -24,11 +41,17 @@ btn.onclick=function(){
             }
         });
     });
-    
 }
-
-    
-let color="black";
+clear.addEventListener("click",()=>{
+    for(let i=0;i<(num*num);i++){
+        arr[i].style.background="white";
+    }
+})
+colorDiv.forEach(element=>{
+element.addEventListener("click",()=>{
+    color=element.getAttribute("class");
+})
+})
 btn2.onclick=function(){
     color=prompt("Type the color you need");
 }
